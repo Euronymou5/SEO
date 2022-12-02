589d2dc57848d6599f8ba60ad87d159c4a5afe34
Euronymou5 <85043356+Euronymou5@users.noreply.github.com>
2022-12-02 14:48:07 -0600

    Added English version.

---
+++ seo_en.py
@@ -0,0 +1,119 @@
+# SEO By: Euronymou5
+# SEO under development (v1.0)
+
+import os
+import requests
+import time
+from googlesearch import search
+import random
+import json
+import re
+from config import intel_api, verify_email
+
+class Colores:
+  rojo = "\033[31m"
+  verde = "\033[32m"
+
+logo_sc = """
+\033[36m      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⢀⣴⣶⣶⣶⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+\033[34m       ⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣧⣝⠛⠛⠃⣾⣷⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+\033[36m⠀⠀      ⠀⠀⠀⠀⠀⠀⣄⡻⠿⠟⠃⢀⣀⣀⣌⣽⣯⡷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+\033[34m⠀⠀⠀⠀      ⠀⠀⠀⠀⣿⣿⡿⠁⠐⠉⠈⣿⣷⡻⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+\033[36m⠀⠀⠀⠀      ⠀⠀⠀⠀⣩⣶⣥⡀⠀⠀⢀⣘⣿⠿⢋⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+\033[34m⠀⠀⠀⠀⠀      ⠀⠀⠀⣿⣯⣷⣶⣦⢠⡞⢡⣿⣻⡿⣻⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀
+\033[36m  ⠀⠀⠀    ⢀⣠⡿⠻⣄⣘⢻⣿⣿⣿⣿⠷⣛⢿⢟⣵⡟⣾⣷⣿⣶⣤⣴⣤⠀⠀⠀
+\033[34m   ⠀⠀   ⣼⠟⣡⣤⣤⣬⣿⣣⣅⣿⣿⣿⣿⣿⣿⣞⠉⣘⣿⠋⡝⠛⢿⣿⣿⣷⣀⠀
+\033[36m⠀      ⢀⡇⢰⣿⢡⡴⠽⣷⣾⡿⠿⣷⣹⡛⠿⢿⣭⡝⠛⠃⠀⠻⠄⠀⠸⣿⣿⣿⣧
+\033[34m      ⠐⠉⠀⠈⣿⢸⡇⢰⡟⠿⠿⠛⠛⠉⡤⢾⠃⣏⠁⠟⠀⠀⠀⠀⣴⣿⠿⠛⣛⡋
+\033[36m⠀      ⠀⠀⠀⣻⢘⡇⢸⠏⠀⣰⣭⠻⠿⣿⠎⠀⠈⠀⠀⠀⠀⠀⠀⡿⠃⠀⣀⣿⠃
+\033[34m⠀⠀      ⠀⠈⠁⠀⣻⠹⣆⢰⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠤⠟⠁⠀
+\033[36m⠀⠀⠀      ⠀⠀⠘⠁⠀⡾⢸⣿⣟⣿⣶⣶⣶⣤⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀
+\033[34m⠀⠀⠀⠀      ⠀⠀⠀⠀⠀⠀⢿⢸⣿⣿⣿⣿⠿⠻⠿⠛⠋⠙⠃⠀⠀⠀⠀⠀⠀⠀
+\033[36m⠀⠀⠀⠀⠀      ⠀⠀⠀⠀⠀⠀⠀⠙⠛⠛⠁⠻⠷⢦⠶⠖⠂⠀⠀⠀⠀⠀⠀⠀⠀
+"""
+logo = """
+      --------------------------------------------
+      |        ███████ ███████  ██████           |
+      |        ██      ██      ██    ██          |
+      |        ███████ █████   ██    ██          |
+      |             ██ ██      ██    ██          |
+      |        ███████ ███████  ██████           |
+      |                                          | 
+      |                v1.0                      |
+      ------ :[ Scorpion Email Osint] : ----------
+"""
+
+dom = ["com","com.tw","co.in","be","de","co.uk","co.ma","dz","ru","ca"]
+
+def main():
+  os.system("clear")
+  print(logo_sc)
+  print(logo)
+  email = input(f'\n{Colores.verde}[-] Enter an email: ')
+  if len(email) == 0 or email == " ":
+    print(f'\n{Colores.rojo}[!] Error you must enter an email.')
+    time.sleep(3)
+    main()
+  else:
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    if (re.fullmatch(regex, email)):
+      print('\n[?] Valid: Valid email')
+    else:
+      print(f'\n{Colores.rojo}[?] Valid: Invalid email')
+      exit()
+    response = requests.get(f'https://verifymail.io/api/{email}?key={verify_email}')
+    data = json.loads(response.content.decode())
+    time.sleep(1)
+    try: 
+      if data['disposable'] == False:
+        print('\n[?] Disposable email: No')
+      else:
+        print('\n[?] Disposable email: Yes')
+      time.sleep(2)
+      print('\n[~] Domain: ', data['domain'])
+      time.sleep(2)
+      if data['block'] == False:
+        print('\n[?] Locked: No')
+      else:
+       print('\n[?] Locked: Si')
+    except KeyError:
+      print(f'{Colores.rojo}\n[!] An error has occurred.')
+    print(f'\n{Colores.verde}[~] Looking for data...')
+    time.sleep(1)
+    # Dorking
+    print(f'\n{Colores.verde}[~] Starting Google search dorking...')
+    tld = random.choice(dom)
+    command = f'intext:{email}' # Simple busqueda de dorking intext
+    command2 = f"site:@ filetype:PDF intext:{email}" # Busqueda de el email en archivos .pdf 
+    command3 = f"site:facebook.com intext:{email}"
+    command4 = f"site:twitter.com intext:{email}"
+    command5 = f"site:instagram.com intext:{email}"
+    for j in search(command, tld, num=10, stop=10, pause=2):
+     print(f'\nResults found!: {j}')
+    print('\n[~] Searching email in pdf files...')
+    time.sleep(4)
+    for i in search(command2, tld, num=10, stop=10, pause=2):
+      print(f'\nResults found!: {i}')
+    print('\n[~] Looking for email on social networks...')
+    for a in search(command3, tld, num=10, stop=10, pause=2):
+      print(f'\nResults found!: {a}')
+    for b in search(command4, tld, num=10, stop=10, pause=2):
+      print(f'\nResults found!: {b}')
+    for c in search(command5, tld, num=10, stop=10, pause=2):
+      print(f'\nResults found!: {c}')
+    # Socialscan
+    print('\n[~] Starting socialscan...')
+    time.sleep(4)
+    print('')
+    os.system(f"socialscan {email}")
+    # H8mail
+    print('\n[~] Starting h8mail...')
+    time.sleep(4)
+    print('')
+    os.system(f"h8mail -t {email}")
+    # Intel X
+    print(f'{Colores.verde}\n[~] Starting Intelligence X...')
+    os.system(f"python3 modules/intelx.py -search {email} -apikey {intel_api}")
+
+    
+main()
